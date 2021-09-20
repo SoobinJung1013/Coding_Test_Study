76c0156e1dcef75f2cbb1c7c0bee54fdf3a5c798
Soobin Jung <judyb0626@gmail.com>
2021-09-20 23:50:42 +0900

    20210920

--- soobinJung/CodingBat/warmup1/1.java
+++ soobinJung/CodingBat/warmup1/1.java
@@ -1,5 +1,8 @@
 class Solution {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        // 1. vacation : true -> sleepIn : true
+        // 2. weekday : false -> sleepIn : true
+        // 3. 예외처리
         if (vacation == false) {
             if (weekday == false) {
                 return true;
@@ -11,21 +14,6 @@ class Solution {
     }
 }
 
-// 내가 생각한 문제 의도
-// 풀이 설명
-// 다른 풀이
-/*
- * public boolean sleepIn(boolean weekday, boolean vacation) { if (!weekday ||
- * vacation) { return true; }
- * 
- * return false;
- * 
- * // Solution notes: better to write "vacation" than "vacation == true" //
- * though they mean exactly the same thing. // Likewise "!weekday" is better
- * than "weekday == false". // This all can be shortened to: return (!weekday ||
- * vacation); // Here we just put the return-false last, or could use an
- * if/else. }
- */
-// 조건문을 한줄로 가돗성있게 써서 중접 조건문 안써도 되게 풀이함 . 아마 빅오퓨ㅕ기법에서 유리할 듯.
 
-// 보통 결과값이 (true, false) 나올 수 있는 경우의 수가 적은걸 모아서 짧게 표현하고 나머지는 ... 이런식으로 표현해줌
+
+
--- soobinJung/CodingBat/warmup1/10.java
+++ soobinJung/CodingBat/warmup1/10.java
@@ -1,13 +1,11 @@
-class Solution {
-    public String missingChar(String str, int n) {
+public String missingChar(String str, int n) {
         // index of array
         // subString(a, b) = a부터 b전까지의 위치의 문자열을 가져온다.
         // substring(a, b)
 
         return str.substring(0, n) + str.substring(n + 1, str.length());
-    }
-
 }
+
 
 // subSting으로 했는데 안됨
 // substring으로 하니까 됨
