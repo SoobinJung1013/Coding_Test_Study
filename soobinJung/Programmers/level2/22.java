2cfb20e5a5e3128af47215d5aead0ee8f72cd337
Soobin Jung <judyb0626@gmail.com>
2021-10-17 14:34:04 +0900

    리팩토링

--- soobinJung/Programmers/level2/22.java
+++ soobinJung/Programmers/level2/22.java
@@ -1,29 +1,65 @@
-import java.util.*;
 import java.util.HashMap;
 
-// 문제는 풀었는데, 속도가 느림
 class Solution {
     public int solution(String[][] clothes) { // 2차원 배열 vs 1차원배열
-        int answer = 0;
-        Map<String, Integer> hm = new HashMap();
-
-        for (int i = 0; i < clothes.length; i++) {
-            hm.put(clothes[i][1], 1);
-        }
-        for (int i = 0; i < clothes.length; i++) {
-            hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
-        }
-
-        int xCal = 1;
-        for (String kinds : hm.keySet()) {
-            xCal = xCal * hm.get(kinds);
-        }
-        answer = answer + xCal;
-        answer = answer - 1;
-
-        return answer;
+        int answer = 1;
+        HashMap<String, Integer> hm = new HashMap();
+        for (int i = 0; i < clothes.length; i++)
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 1) + 1);
+        for (String kinds : hm.keySet())
+            answer *= hm.get(kinds);
+        return answer - 1;
     }
 }
+
+// import java.util.*;
+// import java.util.HashMap;
+
+// // 문제는 풀었는데, 속도가 느림
+// class Solution {
+// public int solution(String[][] clothes) { // 2차원 배열 vs 1차원배열
+// int answer = 0;
+// Map<String, Integer> hm = new HashMap();
+
+// for (int i = 0; i < clothes.length; i++) {
+// hm.put(clothes[i][1], 1);
+// }
+// for (int i = 0; i < clothes.length; i++) {
+// hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
+// }
+
+// int xCal = 1;
+// for (String kinds : hm.keySet()) {
+// xCal = xCal * hm.get(kinds);
+// }
+// answer = answer + xCal;
+// answer += xCal;
+// answer = answer - 1;
+
+// return answer;
+// }
+// }
+
+// import java.util.HashMap;
+
+// class Solution {
+// public int solution(String[][] clothes) { // 2차원 배열 vs 1차원배열
+// int answer = 1;
+// HashMap<String, Integer> hm = new HashMap();
+// for (int i = 0; i < clothes.length; i++) {
+// hm.put(clothes[i][1], 1);
+// }
+// for (int i = 0; i < clothes.length; i++) {
+// hm.put(clothes[i][1], hm.get(clothes[i][1]) + 1);
+// }
+// for (String kinds : hm.keySet()) {
+// answer *= hm.get(kinds);
+// }
+// return answer - 1;
+// }
+// }
+
+// 코드 똑같은데, 길이 줄인거
 
 // import java.util.HashMap;
 // import java.util.Iterator;
