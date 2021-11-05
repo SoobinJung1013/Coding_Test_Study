d01c381fb44caf93d13e714e5037e0fad317c02c
Soobin Jung <judyb0626@gmail.com>
2021-11-05 15:23:18 +0900

    eslint 설치

--- soobinJung/Programmers/level1/23.java
+++ soobinJung/Programmers/level1/23.java
@@ -1,3 +1,12 @@
-public class 23 {
-    
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i])
+                answer += absolutes[i];
+            else
+                answer -= absolutes[i];
+        }
+        return answer;
+    }
 }
